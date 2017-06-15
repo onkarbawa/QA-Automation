a03919c665b45aba82705ce873661cb8872a8c60
bawaonkar <bawa.onkar@tftus.com>
2017-06-15 13:32:07 +0530

    added capabilities

--- cb_customer_app/src/main/java/com/curbside/customerApp/home/HomePage.java
+++ cb_customer_app/src/main/java/com/curbside/customerApp/home/HomePage.java
@@ -1,7 +1,28 @@
 package com.curbside.customerApp.home;
 
+import com.curbside.pages.Page;
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 /**
  * Created by bawa.onkar on 14/06/17.
  */
-public class HomePage {
+public class HomePage extends Page {
+
+    By home = By.id(HomePageUIMap.HOME);
+
+    /**
+     * Gets the instance of the Page
+     *
+     * @param driver
+     */
+    public HomePage(AppiumDriver driver) {
+        super(driver);
+    }
+
+
+    public WebElement getHome() {
+        return this.driver.findElement(home);
+    }
 }
---
+++ cb_customer_app/src/main/java/com/curbside/customerApp/home/HomePageUIMap.java
@@ -0,0 +1,8 @@
+package com.curbside.customerApp.home;
+
+/**
+ * Created by bawa.onkar on 14/06/17.
+ */
+public class HomePageUIMap {
+    static String HOME = "";
+}
