47aabd84125808795eb7549589dec91c016e916f
anil-tftus <kumar.anil@tftus.com>
2017-07-09 23:28:37 +0530

    created abstract screen, all UI classes should extend this

---
+++ cb_customer_app/src/test/java/com/curbside/ios/ui/AbstractScreen.java
@@ -0,0 +1,23 @@
+package com.curbside.ios.ui;
+
+/**
+ * Created by bawa.onkar on 06/07/17.
+ */
+public class AbstractScreen {
+	AccountInfo accountInfoScreen= new AccountInfo();
+	Cart cartScreen= new Cart();
+	CreditCard creditCardScreen= new CreditCard();
+	FacebookLogin facebookLoginScreen= new FacebookLogin();
+	FooterTabs footerTabsScreen= new FooterTabs();
+	Home homeScreen= new Home();
+	Login loginScreen= new Login();
+	LoyalityCard loyalityCardScreen= new LoyalityCard();
+	Map mapScreen= new Map();
+	MyAccount myAccountScreen= new MyAccount();
+	PaymentInfo paymentInfoScreen= new PaymentInfo();
+	ProductDetails productDetailsScreen= new ProductDetails();
+	Search searchScreen= new Search();
+	Settings settingsScreen= new Settings();
+	SignUp signUpScreen= new SignUp();
+	Welcome welcomeScreen= new Welcome();
+}
