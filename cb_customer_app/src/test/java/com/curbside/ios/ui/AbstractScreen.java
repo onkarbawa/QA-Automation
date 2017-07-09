074c43373077b5fe492605cd7da26c1c3a162855
anil-tftus <kumar.anil@tftus.com>
2017-07-10 00:08:16 +0530

    fixing stackoverflow error

--- cb_customer_app/src/test/java/com/curbside/ios/ui/AbstractScreen.java
+++ cb_customer_app/src/test/java/com/curbside/ios/ui/AbstractScreen.java
@@ -6,26 +6,42 @@ import com.curbside.automation.uifactory.UIElement;
  * Created by bawa.onkar on 06/07/17.
  */
 public class AbstractScreen {
-	AccountInfo accountInfoScreen = new AccountInfo();
-	Cart cartScreen = new Cart();
-	CreditCard creditCardScreen = new CreditCard();
-	FacebookLogin facebookLoginScreen = new FacebookLogin();
-	FooterTabs footerTabsScreen = new FooterTabs();
-	Home homeScreen = new Home();
-	Login loginScreen = new Login();
-	LoyalityCard loyalityCardScreen = new LoyalityCard();
-	Map mapScreen = new Map();
-	MyAccount myAccountScreen = new MyAccount();
-	PaymentInfo paymentInfoScreen = new PaymentInfo();
-	ProductDetails productDetailsScreen = new ProductDetails();
-	Search searchScreen = new Search();
-	Settings settingsScreen = new Settings();
-	SignUp signUpScreen = new SignUp();
-	Welcome welcomeScreen = new Welcome();
+	static AccountInfo accountInfoScreen;
+	static Cart cartScreen;
+	static CreditCard creditCardScreen;
+	static FacebookLogin facebookLoginScreen;
+	static FooterTabs footerTabsScreen;
+	static Home homeScreen;
+	static Login loginScreen;
+	static LoyalityCard loyalityCardScreen;
+	static Map mapScreen;
+	static MyAccount myAccountScreen;
+	static PaymentInfo paymentInfoScreen;
+	static ProductDetails productDetailsScreen;
+	static Search searchScreen;
+	static Settings settingsScreen;
+	static SignUp signUpScreen;
+	static Welcome welcomeScreen;
 
-	UIElement loadingIcon = UIElement.byAccessibilityId("In progress");
+	static UIElement loadingIcon = UIElement.byAccessibilityId("In progress");
 
-	AbstractScreen() {
+	static {
+		accountInfoScreen = new AccountInfo();
+		cartScreen = new Cart();
+		creditCardScreen = new CreditCard();
+		facebookLoginScreen = new FacebookLogin();
+		footerTabsScreen = new FooterTabs();
+		homeScreen = new Home();
+		loginScreen = new Login();
+		loyalityCardScreen = new LoyalityCard();
+		mapScreen = new Map();
+		myAccountScreen = new MyAccount();
+		paymentInfoScreen = new PaymentInfo();
+		productDetailsScreen = new ProductDetails();
+		searchScreen = new Search();
+		settingsScreen = new Settings();
+		signUpScreen = new SignUp();
+		welcomeScreen = new Welcome();
 	}
 
 	void waitForScreenToLoad() throws Throwable {
