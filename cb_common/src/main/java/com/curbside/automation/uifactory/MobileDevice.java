5ecc5b766c012873b4fcab126d5c9ce06be5b1f1
nipunsaini <kumar.nipun@tftus.com>
2017-07-11 16:31:15 +0530

    android location grant/revoke permissions

--- cb_common/src/main/java/com/curbside/automation/uifactory/MobileDevice.java
+++ cb_common/src/main/java/com/curbside/automation/uifactory/MobileDevice.java
@@ -83,57 +83,10 @@ public class MobileDevice {
 			}
 
 		} else if(DeviceStore.getPlatform().equalsIgnoreCase("android")) {
-			AndroidDevice.launchSettings();
-
-			int retry = 100;
-			boolean found = false;
-			while (!found && retry > 0) {
-				List<WebElement> list = DriverFactory.getDriver().findElements(By.xpath("//*[@resource-id='com.android.settings:id/dashboard_tile']"));
-				for (WebElement element : list) {
-					//element.findElement(By.className("android.widget.TextView"));
-					if (element.findElement(By.className("android.widget.TextView")).getAttribute("text").equals("Apps")) {
-						element.findElement(By.className("android.widget.TextView")).click();
-						found = true;
-						break;
-					}
-				}
-				if (!found) AndroidDevice.swipeUp();//TODO - not swipe as per screen view
-				retry--;
-			}
-
-			retry = 100;
-			found = false;
-			while (!found && retry > 0) {
-				List<WebElement> apps = DriverFactory.getDriver().findElements(By.xpath("//android.widget.ListView[@index='0']/android.widget.LinearLayout"));
-				for (WebElement app : apps) {
-					if (app.findElement(By.className("android.widget.TextView")).getText().equals(appName)) {
-						app.click();
-						found = true;
-						break;
-					}
-				}
-				if (!found) AndroidDevice.swipeUp();//TODO - not swipe as per screen view
-				retry--;
-			}
-
-			//new UIElement(MobileBy.AndroidUIAutomator("new UiSelector().text(\"Apps\")")).scrollTo().tap();
-			//new UIElement(By.xpath("//android.widget.TextView[@text='Apps']")).scrollTo().tap();
-			//new UIElement(By.xpath("//*[@text='" + appName + "']")).scrollTo().tap();
-			new UIElement(By.xpath("//*[@text='Permissions']")).scrollTo().tap();
-			List<WebElement> permissions = DriverFactory.getDriver().findElements(By.xpath("//android.widget.ListView[@index='0']/android.widget.LinearLayout"));
-			//UIElement locationSwitch = new UIElement(By.xpath("//*[@text='Location']/parent::android.widget.LinearLayout[1]"));
-			for (WebElement p : permissions) {
-				if (p.findElement(By.className("android.widget.TextView")).getAttribute("text").equals("Location")) {
-					WebElement element = p.findElement(By.className("android.widget.Switch"));
-					if (newValue.equals("ON") && element.getAttribute("text").equals("OFF"))
-						element.click();
-					else if (newValue.equals("OFF") && element.getAttribute("text").equals("ON"))
-						element.click();
-					else
-						throw new IllegalArgumentException("No such value");
-					break;
-				}
-			}
+			if (newValue.equalsIgnoreCase("ON"))
+				AndroidDevice.grantLocationPermission(DeviceStore.getDevice().get("appPackage").toString());
+			else
+				AndroidDevice.revokeLocationPermission(DeviceStore.getDevice().get("appPackage").toString());
 		}
 		else
 			throw new NotImplementedException("");
