4fd122b34def577b583ef1c9e0aa106016c6a7fc
anil-tftus <kumar.anil@tftus.com>
2017-07-18 20:17:12 +0530

    emptying cart before adding item

--- cb_customer_app/src/test/java/com/curbside/android/ui/Cart.java
+++ cb_customer_app/src/test/java/com/curbside/android/ui/Cart.java
@@ -3,7 +3,10 @@ package com.curbside.android.ui;
 import com.curbside.automation.common.configuration.Properties;
 import com.curbside.automation.uifactory.UIElement;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.testng.Assert;
@@ -19,9 +22,10 @@ public class Cart extends AbstractScreen {
     UIElement firstRetailer = UIElement.byXpath("//android.support.v7.widget.RecyclerView/android.widget.LinearLayout[@index=‘0’]");
     UIElement loyaltyCardNumber = UIElement.byId("com.curbside.nCurbside:id/text_loyalty_last_4_view");
     UIElement productName = UIElement.byId("com.curbside.nCurbside:id/product_name");
-
-
-
+    UIElement lblCartCount = UIElement.byXpath("//android.widget.TextView[@text='Cart']/../../android.widget.TextView");
+    UIElement btnCartItemQnty = UIElement.byId("com.curbside.nCurbside:id/btn_cart_item_qty");
+    UIElement btnRemove = UIElement.byId("com.curbside.nCurbside:id/button_cart_product_remove");
+    
     @And("^I should see loyalty card info on cart screen$")
     public void iShouldSeeLoyaltyCardInfoOnCartScreen() throws Throwable {
 
@@ -40,5 +44,17 @@ public class Cart extends AbstractScreen {
         footerTabsScreen.tapCart();
         Assert.assertEquals(productName.waitFor(3).getText(), Properties.getVariable("productName"),
           "Added product not shown in the cart");
+    }
+    
+    @Given("^My (C|c)art is empty$")
+    public void emptyCart() throws Throwable
+    {
+    	footerTabsScreen.tapCart();
+    	int itemsInCart= btnCartItemQnty.getCount();
+    	for (int i = 0; i < itemsInCart; i++) {
+			btnCartItemQnty.tap();
+			btnRemove.waitFor(2).tap();
+			Thread.sleep(1000);
+		}
     }
 }
