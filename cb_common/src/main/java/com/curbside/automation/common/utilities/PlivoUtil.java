af6cb853701decbce724dbb5f95750b8ac1a9ddc
h2grover <grover.hitesh@tftus.com>
2017-08-11 00:43:21 +0530

    Refactored the SMS API, Added more functions

--- cb_common/src/main/java/com/curbside/automation/common/utilities/PlivoUtil.java
+++ cb_common/src/main/java/com/curbside/automation/common/utilities/PlivoUtil.java
@@ -80,39 +80,31 @@ public class PlivoUtil {
         return latestSms;
     }
 
-    public static int getInboundMsgCount(String phoneNumber, String GMTDate, boolean upperLimit){
-        int count = 0 ;
-        RestAPI api = new RestAPI("MAMZQ1YWQWZDGYY2E5YT", "YjQ3NjY5ZWFjZWJiM2EwNzBmYjQzNzE2YTNlM2Q3", "v1");
-
+    public static String getLatestMsgToNumber(String authId, String authToken) {
+        String msgToNumber = null;
+        RestAPI api = new RestAPI(authId, authToken, "v1");
         try {
             LinkedHashMap<String, String> parameters = new LinkedHashMap<String, String>();
-            parameters.put("limit", "20");
+            parameters.put("limit", "1");
             parameters.put("offset", "0");
             parameters.put("message_state", "received");
             parameters.put("message_direction", "inbound");
-            parameters.put("message_time__gte", GMTDate);
-            if(upperLimit)
-                parameters.put("message_time__lte", GMTDate);
 
-            // Get count off all the messages according to phone number
+            // Setting filter
             MessageFactory msgFactory = api.getMessages(parameters);
+            msgToNumber = msgFactory.messageList.get(0).toNumber;
 
-            for (Message msg : msgFactory.messageList) {
-
-                if(msg.toNumber.equalsIgnoreCase(phoneNumber)){
-                    ++count;
-                }
-            }
         } catch (PlivoException e) {
             System.out.println(e.getLocalizedMessage());
         }
 
-        return count;
+        return msgToNumber;
     }
 
-    public static int getInboundMsgCount(String phoneNumber, String GMTDate){
+
+    public static int getInboundMsgCount(String authId, String authToken){
         int total_count = 0 ;
-        RestAPI api = new RestAPI("MAMZQ1YWQWZDGYY2E5YT", "YjQ3NjY5ZWFjZWJiM2EwNzBmYjQzNzE2YTNlM2Q3", "v1");
+        RestAPI api = new RestAPI(authId, authToken, "v1");
 
         try {
             LinkedHashMap<String, String> parameters = new LinkedHashMap<String, String>();
@@ -120,10 +112,7 @@ public class PlivoUtil {
             parameters.put("offset", "0");
             parameters.put("message_state", "received");
             parameters.put("message_direction", "inbound");
-            parameters.put("message_time__gte", GMTDate);
 
-            // Get count off all the messages according to phone number
-            MessageFactory msgFactory = api.getMessages(parameters);
             String response = api.request("GET", "/Message/", parameters);
             JsonObject convertedObject = new Gson().fromJson(response, JsonObject.class);
             total_count = Integer.parseInt(convertedObject.get("meta")
@@ -138,32 +127,22 @@ public class PlivoUtil {
         return total_count;
     }
 
-    public static int isSmsReceived(String phoneNumber, String GMTDate){
-        int total_count = 0 ;
-        RestAPI api = new RestAPI("MAMZQ1YWQWZDGYY2E5YT", "YjQ3NjY5ZWFjZWJiM2EwNzBmYjQzNzE2YTNlM2Q3", "v1");
+    public static boolean isSmsReceived(String authId, String authToken, String expectedPhoneNumber, int previousMsgCount){
+        int total_count;
+        String actualToNumber;
+        boolean status;
 
-        try {
-            LinkedHashMap<String, String> parameters = new LinkedHashMap<String, String>();
-            parameters.put("limit", "20");
-            parameters.put("offset", "0");
-            parameters.put("message_state", "received");
-            parameters.put("message_direction", "inbound");
-            parameters.put("message_time__gte", GMTDate);
+        total_count = getInboundMsgCount(authId, authToken);
+        actualToNumber = getLatestMsgToNumber(authId, authToken);
+        if(!actualToNumber.equalsIgnoreCase(expectedPhoneNumber))
+            total_count = -2;
+        if(total_count == previousMsgCount+1)
+            status = true;
+        else
+            status = false;
 
-            // Get count off all the messages according to phone number
-            MessageFactory msgFactory = api.getMessages(parameters);
-            String response = api.request("GET", "/Message/", parameters);
-            JsonObject convertedObject = new Gson().fromJson(response, JsonObject.class);
-            total_count = Integer.parseInt(convertedObject.get("meta")
-                    .getAsJsonObject()
-                    .get("total_count"
-                    ).toString());
 
-        } catch (PlivoException e) {
-            System.out.println(e.getLocalizedMessage());
-        }
-
-        return total_count;
+        return status;
     }
 
 }
