99f4dafb0b3c8b799918a626fa17777ddd13cd3b
bawa.onkar <bawa.onkar@tftus.com>
2017-08-22 14:04:34 +0530

    changes as per new code

--- cb_common/src/main/java/com/curbside/automation/appfactory/AppStore.java
+++ cb_common/src/main/java/com/curbside/automation/appfactory/AppStore.java
@@ -77,8 +77,11 @@ public class AppStore {
 		for (JSONObject appInList : appList) {
 			if(appInList.getString("name").equalsIgnoreCase(appName))
 			{
-				appInList.remove("name");
-				return new JSONObject(appInList.toString());
+				JSONObject app = new JSONObject(appInList.toString());
+
+				app.remove("name");
+				return app;
+					//	new JSONObject(appInList.toString());
 			}
 		}
 		
