001cc79453a72aaf62b4815da7e5e7f33cbd13ea
bawa.onkar <bawa.onkar@tftus.com>
2017-08-30 18:20:20 +0530

    added Live Order Test cases

--- cb_customer_app/src/test/java/com/curbside/ios/ui/GetMyOrder.java
+++ cb_customer_app/src/test/java/com/curbside/ios/ui/GetMyOrder.java
@@ -1,5 +1,6 @@
 package com.curbside.ios.ui;
 
+import com.curbside.automation.uifactory.Steps;
 import com.curbside.automation.uifactory.UIElement;
 import cucumber.api.java.en.And;
 
@@ -12,12 +13,21 @@ public class GetMyOrder extends AbstractScreen {
     UIElement addressSearchField = UIElement.byXpath("//XCUIElementTypeSearchField[@name='Search']");
     UIElement savedAddress = UIElement.byXpath("//XCUIElementTypeTable//XCUIElementTypeCell");
     UIElement addressSuggestions = UIElement.byXpath("//XCUIElementTypeTable//XCUIElementTypeCell");
+    UIElement deliveryNotAvailable = UIElement.byName("Delivery Not Available");
 
     @And("^I select the delivery address as:\"([^\"]*)\"$")
     public void iSelectTheDeliveryAddressAsStreetCityState(String address) throws Throwable {
         int totalAddress = savedAddress.waitFor(5).getCount();
         if (totalAddress >1){
             savedAddress.tap();
+
+            if (deliveryNotAvailable.isDisplayed()){
+                Steps.tapButton("OK");
+                addNewAddressButton.tap();
+                addressSearchField.waitFor(5).sendKeys("4 Palo Alto Square, Palo Alto,California 94306", false);
+
+                addressSuggestions.waitFor(5).tap();
+            }
         }
         else {
             addNewAddressButton.tap();
--- cb_customer_app/src/test/java/com/curbside/ios/ui/MailNotification.java
+++ cb_customer_app/src/test/java/com/curbside/ios/ui/MailNotification.java
@@ -2,6 +2,7 @@ package com.curbside.ios.ui;
 
 import com.curbside.automation.common.configuration.Properties;
 import com.curbside.automation.common.utilities.Mailinator;
+import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import org.testng.Assert;
 
@@ -14,30 +15,48 @@ public class MailNotification {
     public void iCheckLatestEmail(String emailType) throws Throwable {
         String emailID;
 
-       emailID = Properties.getVariable("signupEmail");
-       emailID = emailID.split("@")[0];
-
-
-
         switch (emailType.toLowerCase()) {
             case "welcome":
+                emailID = Properties.getVariable("signupEmail");
+                emailID = emailID.split("@")[0];
                 Assert.assertEquals(Mailinator.isMailReceived(emailID, "Welcome to curbside"), true,
                         "Signup mail not received yet");
                 break;
 
+            case "progress":
+                emailID = Properties.getVariable("signupEmail");
+                emailID = emailID.split("@")[0];
+                Assert.assertEquals(Mailinator.isMailReceived(emailID, "We’re Prepping Your Curbside Pickup Order"),
+                        true,"In Progress mail not received yet");
+                break;
+
             case "in-progress":
-                Assert.assertEquals(Mailinator.isMailReceived("palo_alto", "We’re Prepping Your Curbside Pickup Order"),
+                Assert.assertEquals(Mailinator.isMailReceived("live_order", "We’re Prepping Your Curbside Pickup Order"),
                         true,"In Progress mail not received yet");
                 break;
 
             case "delivery-order":
-                Assert.assertEquals(Mailinator.isMailReceived("palo_alto", "We’re Prepping Your Delivery Order"),
+                Assert.assertEquals(Mailinator.isMailReceived("live_order", "We’re Prepping Your Delivery Order"),
+                        true,"Delivery Order mail not received yet");
+                break;
+
+            case "in-progress curbside pickup":
+                Assert.assertEquals(Mailinator.isMailReceived("palo_alto", "We’re Prepping Your Curbside Pickup Order"),
                         true,"In Progress mail not received yet");
+                break;
+
+            case "in-progress delivery-order":
+                Assert.assertEquals(Mailinator.isMailReceived("palo_alto", "We’re Prepping Your Delivery Order"),
+                        true,"Delivery Order mail not received yet");
                 break;
 
             default:
                 Assert.fail("Please rectify the correct email type in the feature file ");
         }
+    }
 
+    @Given("^The mail box of userID \"([^\"]*)\" is empty$")
+    public void theMailBoxOfUserIDIsEmpty(String userID) throws Throwable {
+        Mailinator.deleteMails(userID);
     }
 }
