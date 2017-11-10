76a1aac91377e0411c2ef9c47eb2bb96556e1a44
h2grover <grover.hitesh@tftus.com>
2017-11-10 18:41:10 +0530

    Refactored IOS CAP EE

--- cb_customer_app/src/test/java/com/capcvs/ios/runners/ErrorMessageTest.java
+++ cb_customer_app/src/test/java/com/capcvs/ios/runners/ErrorMessageTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 @CucumberOptions(
         monochrome = true,
         features = "src/test/resources/capcvs/features/ios/ErrorMessage.feature",
-        plugin = {"json:target/cucumber-report.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report_cap_cvs.html"},
+        plugin = {"json:target/cucumber-report.json", "com.cucumber.listener.ExtentCucumberFormatter:output/report.html"},
         format = { "pretty","html: cucumber-html-reports",
                 "json: cucumber-html-reports/cucumber.json" },
         dryRun = false, strict= true,
